251574b22332e8bbf59589a8c747d2309c0c9fab
nily <nily>
2025-10-10 17:11:51 -0500

    Update Footer component background color for improved visibility

--- src/components/ui/layout/Footer.tsx
+++ src/components/ui/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center w-full bg-purple-950/30 backdrop-blur-sm px-6 py-5 border-t-[1px] border-purple-300/30">
+    <footer className="flex justify-center w-full bg-purple-950/10 backdrop-blur-sm px-6 py-5 border-t-[1px] border-purple-300/30">
       <p className="text-white">
         © 2025 Personal Budget. Budget friendly, life ready.
       </p>
