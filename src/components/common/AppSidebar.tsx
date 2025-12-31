7608813dd201213f83a2c9ee2a889342a06c477e
nily <nily>
2025-12-31 00:23:26 -0500

    updates

--- src/components/common/AppSidebar.tsx
+++ src/components/common/AppSidebar.tsx
@@ -42,7 +42,7 @@ const items = [
 
 export function AppSidebar() {
   return (
-    <Sidebar collapsible="icon">
+    <Sidebar>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
