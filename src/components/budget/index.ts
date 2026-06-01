822163c6fa8be7f5bba32c36f38ca63331b2cac3
nily <nily>
2026-06-01 00:50:50 -0500

    feat(budget): add OnboardingStepper component

--- src/components/budget/index.ts
+++ src/components/budget/index.ts
@@ -1,4 +1,5 @@
 export { BudgetForm } from './budget-form'
+export { OnboardingStepper } from './onboarding-stepper'
 export { OverviewSkeleton } from './overview-skeleton'
 export { PeriodOption } from './period-option'
 export { PeriodSelector } from './period-selector'
